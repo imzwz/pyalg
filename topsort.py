f2251b137259e9d4e6daeafc950ffcf7aef23999
winn <iamzwz3@gmail.com>
2017-03-11 10:15:56 +0800

    update topsort add dfs version

--- topsort.py
+++ topsort.py
@@ -27,6 +27,18 @@ def topsort(G):
                 Q.append(v)
     return S
 
+def dfs_topsort(G):
+    S, res = set(), []
+    def recurse(u):
+        if u in S: return 
+        S.add(u)
+        for v in G[u]:
+            recurse(v)
+        res.append(u)
+    for u in G:
+        recurse(u)
+    res.reverse()
+    return res
 
         
 
