bf330a4cf03b88adf285f270afd29e45704260b8
winn <iamzwz3@gmail.com>
2017-03-13 15:30:53 +0800

    update kruskal algorithm

--- kruskal.py
+++ kruskal.py
@@ -18,3 +18,30 @@ def naive_kruskal(G):
             naive_union(C, u, v)
     return T
 
+def find(C, u):
+    if C[u] != u:
+        C[u] = find(C, C[u])
+    return C[u]
+
+def union(C, R, u, v):
+    u, v = find(C,u), find(C,v)
+    if R[u] > R[v]:
+        C[v]=u
+    else:
+        C[u]=v
+    if R[u]==R[v]:
+        R[v]+=1
+
+def kruskal(G):
+    E = [(G[u][v], u, v) for u in G for v in G[u]]
+    T = set()
+    C, R = {u:u for u in G}, {u:0 for u in G}
+    for _, u, v in sorted(E):
+        if find(C, u)!=find(C, v):
+            T.add((u, v))
+            union(C, R, u, v)
+    return T
+
+        
+
+
