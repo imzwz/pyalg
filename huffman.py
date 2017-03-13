957690fe61f852ce6c3c8cd2d5e8f59eb126fb24
winn <iamzwz3@gmail.com>
2017-03-13 14:54:39 +0800

    add python huffman

---
+++ huffman.py
@@ -0,0 +1,14 @@
+from heapq import heapify, heappush, heappop
+from itertools import count
+
+def huffman(seq, frq):
+    num = count()
+    trees = list(zip(frq, num, seq))
+    heapify(trees)
+    while len(trees) >1:
+        fa, _, a = heappop(trees)
+        fb, _, b = heappop(trees)
+        n = next(num)
+        heappush(trees, (fa+fb, n, [a,b]))
+    return trees[0][-1]
+
