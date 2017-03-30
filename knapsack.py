7b8892dcb133df89eed120a9a2b11e6217e17a03
winn <iamzwz3@gmail.com>
2017-03-30 10:33:21 +0800

    add knapsack problem

---
+++ knapsack.py
@@ -0,0 +1,20 @@
+def rec_unbounded_knapsack(w,v,c):
+    @memo
+    def m(r):
+        if r==0:
+            return 0
+        val = m(r-1)
+        for i, wi in enumerate(w):
+            if wi > r: continue
+            val = max(val, v[i]+m(r-wi))
+    return m(c)        
+
+def unbounded_knapsack(w, v, c):
+    m = [0]
+    for r in range(1, c+1):
+        val = m[r-1]
+        for i, wi in enumerate(w):
+            if wi > r: continue
+            val = max(val, v[i]+m[r-wi])
+        m.append(val)
+    return m[c]
