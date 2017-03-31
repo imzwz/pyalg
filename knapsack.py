9f86f9274deeb69054918b36e2fbd832a7df9b00
winn <iamzwz3@gmail.com>
2017-03-31 10:03:31 +0800

    update python knapsack add 0-1 knapsack

--- knapsack.py
+++ knapsack.py
@@ -18,3 +18,32 @@ def unbounded_knapsack(w, v, c):
             val = max(val, v[i]+m[r-wi])
         m.append(val)
     return m[c]
+
+def rec_knapsack(w, v, c):
+    @memo
+    def m(k,r):
+        if k==0 or r==0 :
+            return 0
+        i = k-1
+        drop = m(k-1, r)
+        if w[i] > r: 
+            return drop
+        return max(drop, v[i] + m(k-1, r-w[i]))
+    return m(len(w), c)
+
+def knapsack(w, v, c):
+    n = len(w)
+    m = [[0]*(c+1) for i in range(n+1)]
+    p = [[False]*(c+1) for i in range(n+1)]
+    for k in range(1, n+1):
+        i = k-1
+        for r in range(1, c+1):
+            m[k][r] = drop = m[k-1][r]
+            if w[i] > r: continue
+            keep = v[i] + m[k-1][r-w[i]]
+            m[k][r] = max(drop, keep)
+            p[k][r] = keep > drop
+    return m,p
+
+
+
