7164a62c02bb246a545221610000cfefad3c5a7b
winn <iamzwz3@gmail.com>
2017-04-13 20:22:34 +0800

    update python quicksort

--- quicksort.py
+++ quicksort.py
@@ -9,3 +9,27 @@ def quicksort(seq):
     lo, pi, hi = partition(seq)
     return quicksort(lo) + [pi] + quicksort(hi)
 
+def qsort(seq,low=0,high=None):
+    if high==None:
+        high = len(seq)-1
+    if low < high:
+        s, i, j = seq[low],low,high
+        while i < j:
+            while i < j and seq[j] >=s:
+                j=j-1
+            if i<j:
+                seq[i]=seq[j]
+                i=i+1
+            while i < j and seq[i] <=s:
+                i=i+1
+            if i<j:
+                seq[j] = seq[i]
+                j=j-1
+        seq[i] = s
+        qsort(seq, low, i-1)
+        qsort(seq, i+1, high)
+
+seq = [3,1,9,5,2,4]
+qsort(seq)
+print(seq)
+
